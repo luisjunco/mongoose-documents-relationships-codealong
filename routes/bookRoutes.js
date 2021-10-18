26665b76ea261234cbebff3cb7b3b66c92d48705
Luis Junco <38411763+ludovico-wd@users.noreply.github.com>
2021-10-18 18:05:45 +0200

    (CREATE) Step 4.b: receive the information from the form and save in DB

--- routes/bookRoutes.js
+++ routes/bookRoutes.js
@@ -21,6 +21,42 @@ router.get('/books/create', (req, res, next) => {
 });
 
 
+router.post("/books/create", (req, res, next) => {
+
+    /* OPTION 1 */
+
+    // const bookTitle = req.body.title;
+    // const bookAuthor = req.body.author;
+    // const bookDescription = req.body.description;
+    // const bookRating = req.body.rating;
+
+    // const data = {
+    //     title: bookTitle,
+    //     description: bookDescription,
+    //     author: bookAuthor,
+    //     rating: bookRating
+    // }
+
+    // Book.create(data)
+    //     .then()
+    //     .catch();
+
+
+    /* OPTION 2 */
+    
+    const {title, author, description, rating} = req.body;
+    Book.create({title, author, description, rating})
+        .then( () => {
+            res.redirect("/books");
+        })
+        .catch( (error) => {
+            console.log("Error adding new book to DB", error);
+            next(error);
+        });
+
+})
+
+
 router.get("/books/:bookId", (req, res, next) => {
     Book.findById(req.params.bookId)
         .then( (bookFromDB) => {
