d4620428134cc9fbbe3a168f759437db4ffb8280
Luis Junco <38411763+ludovico-wd@users.noreply.github.com>
2021-10-19 17:21:46 +0200

    Step 0: update DB name

--- bin/seeds.js
+++ bin/seeds.js
@@ -3,7 +3,7 @@
 const mongoose = require('mongoose');
 const Book = require('../models/Book.model');
 
-const MONGO_URI = process.env.MONGODB_URI || 'mongodb://localhost/library-project';
+const MONGO_URI = process.env.MONGODB_URI || 'mongodb://localhost/library-v2';
 
 mongoose.connect(MONGO_URI, {
   useNewUrlParser: true,
